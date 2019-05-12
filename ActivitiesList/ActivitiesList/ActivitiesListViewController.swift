8528b415704785b125bb9a57e13ec305b9e65fc4
Macintosh <macintosh@MacBook-Air-Macintosh.local>
2019-05-12 21:16:14 +0300

    Adding table cells with checkmarks

--- ActivitiesList/ActivitiesList/ActivitiesListViewController.swift
+++ ActivitiesList/ActivitiesList/ActivitiesListViewController.swift
@@ -15,6 +15,38 @@ class ActivitiesListViewController: UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1000
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ActivitiesListItem", for: indexPath)
+        
+        if let label = cell.viewWithTag(1000) as? UILabel {
+            if indexPath.row % 5 == 0 {
+                label.text = "Go to gym"
+            } else if indexPath.row % 5 == 1 {
+                label.text = "Read the book"
+            } else if indexPath.row % 5 == 2 {
+                label.text = "Go to date"
+            } else if indexPath.row % 5 == 3 {
+                label.text = "Play Uncharted 2"
+            } else if indexPath.row % 5 == 4 {
+                label.text = "Study English"
+            }
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
