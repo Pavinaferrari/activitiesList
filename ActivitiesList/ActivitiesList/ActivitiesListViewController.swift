9ddc2d5ef5977e6385f86a8b8655259553c20c85
Macintosh <macintosh@MacBook-Air-Macintosh.local>
2019-05-12 19:42:56 +0300

    Creating an empty table view app

---
+++ ActivitiesList/ActivitiesList/ActivitiesListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActivitiesList
+//
+//  Created by Macintosh on 12.05.2019.
+//  Copyright © 2019 PavluhaCo. All rights reserved.
+//
+
+import UIKit
+
+class ActivitiesListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
